ff1409acd797aacdc60cb36edaf9b72a8918ccc7
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-10 17:29:03 +0800

    chore(backend): update migration configurations
    - Specify migration file extension in knexfile.js
    - Add migrate:latest and migrate:rollback scripts

--- apps/backend/knexfile.js
+++ apps/backend/knexfile.js
@@ -6,12 +6,18 @@ export default {
 		client: 'better-sqlite3',
 		useNullAsDefault: true,
 		connection: { filename: './dev.sqlite3' },
-		migrations: { directory: './migrations' }
+		migrations: {
+			directory: './migrations',
+			extension: 'ts'
+		}
 	},
 	production: {
 		client: 'better-sqlite3',
 		useNullAsDefault: true,
 		connection: { filename: './prod.sqlite3' },
-		migrations: { directory: './migrations' }
+		migrations: {
+			directory: './migrations',
+			extension: 'ts'
+		}
 	}
 };
