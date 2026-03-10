78b85052190a0d1ec3ffee4272f80ea558563b36
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-03-10 20:53:12 +0800

    feat(eslint): add custom rules for padding-line-between-statements

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -10,5 +10,29 @@ export default defineConfig([
 		extends: ['js/recommended'],
 		languageOptions: { globals: globals.node }
 	},
-	tseslint.configs.recommended
+	tseslint.configs.recommended,
+	{
+		rules: {
+			'padding-line-between-statements': [
+				'error',
+				{ blankLine: 'always', prev: 'import', next: '*' },
+				{ blankLine: 'never', prev: 'import', next: 'import' },
+				{ blankLine: 'always', prev: 'export', next: '*' },
+				{ blankLine: 'always', prev: '*', next: 'export' },
+				{ blankLine: 'always', prev: ['var', 'let', 'const'], next: '*' },
+				{ blankLine: 'always', prev: '*', next: ['var', 'let', 'const'] },
+				{
+					blankLine: 'never',
+					prev: ['var', 'let', 'const'],
+					next: ['var', 'let', 'const']
+				},
+				{ blankLine: 'always', prev: 'block-like', next: '*' },
+				{ blankLine: 'always', prev: '*', next: 'block-like' },
+				{ blankLine: 'never', prev: 'if', next: 'if' },
+				{ blankLine: 'any', prev: 'for', next: 'for' },
+				{ blankLine: 'never', prev: 'expression', next: 'expression' },
+				{ blankLine: 'never', prev: '*', next: 'return' }
+			]
+		}
+	}
 ]);
